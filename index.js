b2aa76645e0104950310821559e850b8aaf7cb1c
Ed <edlym96@hotmail.com>
2020-09-07 00:02:55 +0800

    fix

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-$(document).ready(function(){
+let lat = -1
+let long = -1
 
-	lat = -1
-	long = -1
+$(document).ready(function(){
 	
+	let settings;
+
 	if (navigator.geolocation) {
 	  navigator.geolocation.getCurrentPosition(showPosition, showError);
 	} else {
@@ -14,6 +16,16 @@ $(document).ready(function(){
 	  long = position.coords.longitude
 	  console.log(lat)
 	  console.log(long)
+	  settings = {
+			"async": true,
+			"crossDomain": true,
+			"url": "https://developers.zomato.com/api/v2.1/search?lat="+lat+"&lon="+long+"&radius=20000",
+			"method": "GET",
+			"headers": {
+				"Accept": "application/json",
+				"user-key": "df955f8b62ed3879394c22edb2dd71ac"
+				}
+		}	
 	}
 
 	function showError(error) {
@@ -33,17 +45,6 @@ $(document).ready(function(){
   		}
 
   		$("#test-btn").attr('disabled', true)
-	}
-
-	var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://developers.zomato.com/api/v2.1/search?lat="+lat+"&lon="+long+"&radius=20000",
-	"method": "GET",
-	"headers": {
-		"Accept": "application/json",
-		"user-key": "df955f8b62ed3879394c22edb2dd71ac"
-		}
 	}
 
 	$('#test-btn').click(function(e){
